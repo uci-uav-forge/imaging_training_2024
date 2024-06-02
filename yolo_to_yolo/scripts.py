f23bcb36cb4d5652e62ccf33f04925427208ba6f
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-06-02 15:02:35 -0700

    feat: add padding to script

--- yolo_to_yolo/scripts.py
+++ yolo_to_yolo/scripts.py
@@ -8,7 +8,7 @@ def crop_targets(
     input_dir: Path,
     output_dir: Path
 ) -> None:
-    pipeline = YoloDataPipeline([BBoxToCropTransformer()])
+    pipeline = YoloDataPipeline([BBoxToCropTransformer(min_padding=5)])
     pipeline.apply_to_dir(input_dir, output_dir)
 
 
@@ -16,6 +16,6 @@ if __name__ == '__main__':
     datasets_dir = Path("/home/minh/Desktop/uavf_2024/imaging_training_2024/data")
     
     crop_targets(
-        Path(datasets_dir / "crop_bug_min_reproduce"),
-        Path(datasets_dir / "isaac_godot_processed")
+        Path(datasets_dir / "godot_4000_all_labels"),
+        Path(datasets_dir / "godot_4000_cropped")
     )
