1166f749908200c007699f8609744554eeca24b9
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-06-08 22:39:42 -0700

    feat: add note for absolute path

--- general_classifier/default_settings.py
+++ general_classifier/default_settings.py
@@ -2,8 +2,8 @@
 Settings file for training general classifier.
 """
 
-
-DATA_YAML: str = "data.yaml"
+# NOTE: Use absolute path
+DATA_YAML: str = "/home/minh/data/godot_4000_cropped_64/data.yaml"
 BATCH_SIZE: int = 32
 EPOCHS: int = 100
 
