25f381c87afa6056d2785923f0ceee087bc606ce
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-06-17 20:40:39 -0700

    feat: increase batch size

--- general_classifier/default_settings.py
+++ general_classifier/default_settings.py
@@ -4,7 +4,7 @@ Settings file for training general classifier.
 
 # NOTE: Use absolute path
 DATA_YAML: str = "/home/minh/data/godot_4000_cropped/data.yaml"
-BATCH_SIZE: int = 128
+BATCH_SIZE: int = 512
 EPOCHS: int = 100
 
 LOGS_PATH: str = "lightning_logs"
