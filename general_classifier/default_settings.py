b6f45055f71ec03ae395d796ba51e7cf94630898
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-06-24 13:46:10 -0700

    feat: better training defaults

--- general_classifier/default_settings.py
+++ general_classifier/default_settings.py
@@ -4,8 +4,8 @@ Settings file for training general classifier.
 
 # NOTE: Use absolute path
 DATA_YAML: str = "/home/minh/data/godot_4000_cropped/data.yaml"
-BATCH_SIZE: int = 512
-EPOCHS: int = 100
+BATCH_SIZE: int = 256
+EPOCHS: int = 1000
 
 LOGS_PATH: str = "lightning_logs"
 
